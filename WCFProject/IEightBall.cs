3db45208b4e9d5f445498fb8b2f3c4b2bc0d206e
savemysoul382 <serjke88@gmail.com>
2020-04-14 19:04:55 +0300

    init WCF service

---
+++ WCFProject/IEightBall.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace WCFProject
+{
+    [ServiceContract(Namespace = "http://MyCompany.com")]
+    public interface IEightBall
+    {
+        // Задайте вопрос, получите ответ!
+        [OperationContract]
+        String ObtainAnswerToQuestion(String userQuestion);
+    }
+}
---
+++ WCFProject/MagicEightBallService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WCFProject
+{
+    public class MagicEightBallService : IEightBall
+    {
+        public MagicEightBallService()
+        {
+            Console.WriteLine("The 8-Ball awaits your question...");
+        }
+
+        public String ObtainAnswerToQuestion(String userQuestion)
+        {
+            String[] answers = { "Future Uncertain", "Yes", "No",
+                "Hazy", "Ask again later", "Definitely" };
+            // Возвратить случайный ответ.
+            Random r = new Random();
+            return answers[r.Next(answers.Length)];
+        }
+    }
+}
