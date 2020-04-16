662d66c523d529eb370ba693440942c2fd8af2a5
savemysoul382 <serjke88@gmail.com>
2020-04-16 15:57:35 +0300

    + MagicEightBallServiceHost Library + configure App.Config for service_host + open service host and start listening on port 8080

---
+++ MagicEightBallServiceHost/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using WCFProject;
+
+
+namespace MagicEightBallServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Console Based WCF Host *****");
+            using (ServiceHost serviceHost = new ServiceHost(typeof(MagicEightBallService)))
+            {
+                // Открыть хост и начать прослушивание входящих сообщений.
+                serviceHost.Open();
+                // Оставить службу функционирующей до тех пор,
+                // пока не будет нажата клавиша <Enter>.
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press the Enter key to terminate service.");
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
